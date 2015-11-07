3b559d39e720b81df607e37481245c35c4cafa3d
Michal Young <michal.young@gmail.com>
2015-11-07 12:37:22 -0800

    Fixed initializaton of host, client secret

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import flask
 from flask import render_template
 from flask import request
 from flask import url_for
+import uuid
 
 import json
 import logging
@@ -343,17 +344,19 @@ def format_arrow_time( time ):
 
 
 if __name__ == "__main__":
-    # App is created above so that it will
-    # exist whether this is 'main' or not
-    # (e.g., if we are running in a CGI script)
-    app.debug=CONFIG.DEBUG
-    app.logger.setLevel(logging.DEBUG)
-    # We run on localhost only if debugging,
-    # otherwise accessible to world
-    if CONFIG.DEBUG:
-        # Reachable only from the same computer
-        app.run(port=CONFIG.PORT)
-    else:
-        # Reachable from anywhere 
-        app.run(port=CONFIG.PORT,host="0.0.0.0")
+  # App is created above so that it will
+  # exist whether this is 'main' or not
+  # (e.g., if we are running in a CGI script)
+
+  app.secret_key = str(uuid.uuid4())  
+  app.debug=CONFIG.DEBUG
+  app.logger.setLevel(logging.DEBUG)
+  # We run on localhost only if debugging,
+  # otherwise accessible to world
+  if CONFIG.DEBUG:
+    # Reachable only from the same computer
+    app.run(port=CONFIG.PORT)
+  else:
+    # Reachable from anywhere 
+    app.run(port=CONFIG.PORT,host="0.0.0.0")
     
